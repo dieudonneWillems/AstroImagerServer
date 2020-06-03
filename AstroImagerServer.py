92736d0ecfaaf4932439e30bd27777270cc3e2bb
Dieudonne Willems <don.willems@wur.nl>
2020-06-03 17:38:49 +0200

    Started class for Astro Imager Server incorporating an INDI client.

--- AstroImagerServer.py
+++ AstroImagerServer.py
@@ -2,58 +2,79 @@ import time
 
 import PyIndi
 
-class IndiClient(PyIndi.BaseClient):
+class AstroImagerServer(PyIndi.BaseClient):
+
+    cmonitor = None
+
+    '''
+    Initialises a new Astro Imager server.
+    '''
     def __init__(self):
-        super(IndiClient, self).__init__()
+        super(AstroImagerServer, self).__init__()
+        self.setServer("localhost", 7624)
+
+    def connectToINDIServer(self):
+        self.connectServer()
+        # wait CONNECTION property be defined
+        while not(self.cmonitor):
+            time.sleep(0.05)
+    '''
+    INDI Base Client methods - receiving INDI data
+    '''
     def newDevice(self, d):
         global dmonitor
         # We catch the monitored device
         dmonitor=d
         print("New device ", d.getDeviceName())
+
     def newProperty(self, p):
         global monitored
         global cmonitor
         # we catch the "CONNECTION" property of the monitored device
-        if (p.getDeviceName()==monitored and p.getName() == "CONNECTION"):
+        if (p.getName() == "CONNECTION"):
             cmonitor=p.getSwitch()
         print("New property ", p.getName(), " for device ", p.getDeviceName())
+
     def removeProperty(self, p):
         pass
+
     def newBLOB(self, bp):
         pass
+
     def newSwitch(self, svp):
         pass
+
     def newNumber(self, nvp):
         global newval
         global prop
         # We only monitor Number properties of the monitored device
         prop=nvp
         newval=True
+
     def newText(self, tvp):
         pass
+
     def newLight(self, lvp):
         pass
+
     def newMessage(self, d, m):
         pass
+
     def serverConnected(self):
         pass
+
     def serverDisconnected(self, code):
         pass
 
 monitored="Telescope Simulator"
 dmonitor=None
-cmonitor=None
 
-indiclient=IndiClient()
-indiclient.setServer("localhost",7624)
+server = AstroImagerServer()
+server.connectToINDIServer()
 
 # we are only interested in the telescope device properties
-indiclient.watchDevice(monitored)
-indiclient.connectServer()
+#indiclient.watchDevice(monitored)
 
-# wait CONNECTION property be defined
-while not(cmonitor):
-    time.sleep(0.05)
 
 # if the monitored device is not connected, we do connect it
 if not(dmonitor.isConnected()):
@@ -62,7 +83,7 @@ if not(dmonitor.isConnected()):
     # each element of the "CONNECTION" vector is a ISwitch
     cmonitor[0].s=PyIndi.ISS_ON  # the "CONNECT" switch
     cmonitor[1].s=PyIndi.ISS_OFF # the "DISCONNECT" switch
-    indiclient.sendNewSwitch(cmonitor) # send this new value to the device
+    server.sendNewSwitch(cmonitor) # send this new value to the device
 
 newval=False
 prop=None
